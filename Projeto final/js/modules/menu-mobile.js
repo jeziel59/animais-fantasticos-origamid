c04e4ce7ac92061d3caea27e7a3bf7cc879ed307
Jeziel Silva <gearqq@hotmail.com>
2023-05-18 15:21:03 -0300

    menu mobile refatorado

--- Projeto final/js/modules/menu-mobile.js
+++ Projeto final/js/modules/menu-mobile.js
@@ -1,20 +1,33 @@
 import outsideClick from './outsideclick.js';
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ['click', 'touchstart'];
+export default class MenuMobile {
+  constructor(menuButton, menuList, events) {
+  this.menuButton = document.querySelector(menuButton);
+  this.menuList = document.querySelector(menuList);
+  this.classActive = 'active'
+  if (events === undefined) this.events = ["touchstart", "click"];
+  else this.events = events;
 
-  function openMenu() {
-    menuList.classList.add('active');
-    menuButton.classList.add('active');
-    outsideClick(menuList, eventos, () => {
-      menuList.classList.remove('active');
-      menuButton.classList.remove('active');
+  this.openMenu = this.openMenu.bind(this)
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.classActive);
+    this.menuButton.classList.add(this.classActive);
+    outsideClick(this.menuList, this.events, () => { // ativa metodo externo de verificação de clique fora
+      this.menuList.classList.remove(this.classActive);// caso seja fora remove as classes
+      this.menuButton.classList.remove(this.classActive);
     });
   }
 
-  if (menuButton) {
-    eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  addEventOpenMenu() {
+      if (this.menuButton)this.events.forEach(evento => this.menuButton.addEventListener(evento, this.openMenu))
+  }
+
+  init() {
+    if(this.menuButton && this.menuList){
+    this.addEventOpenMenu()
+  }
+return this
   }
 }
